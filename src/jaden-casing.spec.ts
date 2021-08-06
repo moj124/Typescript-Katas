0e2892472f8d462bd11d8a40d257adef1abb2f19
michael jones <michael.jowen.jones@gmail.com>
2021-08-06 13:09:29 +0100

    d

--- src/jaden-casing.spec.ts
+++ src/jaden-casing.spec.ts
@@ -1,6 +1,7 @@
 import "./jaden-casing";
 
 test("Capitalize the first letter of each word", () => {
-  let str: string = (`How can mirrors be real if our eyes aren't real`);
-  expect(str.toJadenCase()).toBe("How Can Mirrors Be Real If Our Eyes Aren't Real");
+  expect(`How can mirrors be real if our eyes aren't real`.toJadenCase()).toBe(
+    "How Can Mirrors Be Real If Our Eyes Aren't Real",
+  );
 });
--- src/jaden-casing.ts
+++ src/jaden-casing.ts
@@ -1,24 +1,14 @@
-
-
-
 declare global {
-    interface String {
-      padZero(length: number): string;
-      toJadenCase(): string;
-    }
+  interface String {
+    toJadenCase(): string;
   }
+}
 
-String.prototype.toJadenCase = function () : string {
-    const arr = this.toString().split(' ').map((element) => element.charAt(0).toUpperCase() + element.slice(1));
-    return arr.join(' ');
+String.prototype.toJadenCase = function (): string {
+  const arr = this.toString()
+    .split(" ")
+    .map((element) => element.charAt(0).toUpperCase() + element.slice(1));
+  return arr.join(" ");
 };
 
-String.prototype.padZero = function (length: number) {
-    let d = String(this)
-    while (d.length < length) {
-      d = '0' + d;
-    }
-    return d;
-  };
-
-export {}
+export {};
